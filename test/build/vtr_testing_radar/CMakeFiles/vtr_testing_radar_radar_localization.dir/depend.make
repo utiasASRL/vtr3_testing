555d6093d751a9e5e8c0e5efe3b82a55ff7a4c77
PioneerSam <pioneerqxy@gmail.com>
2025-05-29 16:20:41 -0400

    reset main

--- test/build/vtr_testing_radar/CMakeFiles/vtr_testing_radar_radar_localization.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for vtr_testing_radar_radar_localization.
-# This may be replaced when dependencies are built.
--- test/build/vtr_testing_radar/CMakeFiles/vtr_testing_radar_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vtr_testing_radar_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vtr_testing_radar_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
